81b4ded9ff879cd4f44e5a4adca9e475713d3d8b
Mikhail Bautin <mbautin@gmail.com>
2013-03-14 19:06:21 -0700

    Set maxRows to unlimited in sql() and sql2rdd()

--- src/main/scala/shark/SharkContext.scala
+++ src/main/scala/shark/SharkContext.scala
@@ -76,7 +76,9 @@ class SharkContext(
     if (proc.isInstanceOf[Driver]) {
       val driver: Driver =
         if (SharkConfVars.getVar(hiveconf, SharkConfVars.EXEC_MODE) == "shark") {
-          new SharkDriver(hiveconf)
+          val newDriver = new SharkDriver(hiveconf)
+          newDriver.setMaxRows(Int.MaxValue)
+          newDriver
         } else {
           proc.asInstanceOf[Driver]
         }
@@ -106,6 +108,7 @@ class SharkContext(
     SparkEnv.set(sparkEnv)
     SessionState.start(sessionState)
     val driver = new SharkDriver(hiveconf)
+    driver.setMaxRows(Int.MaxValue)
     try {
       driver.init()
       driver.tableRdd(cmd)
